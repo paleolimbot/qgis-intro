b84bc3bbf72c3a71937d349c43093553d9758b09
Dewey Dunnington <dewey@fishandwhistle.net>
2018-04-27 23:26:23 -0300

    move deploy strategy to local machine

---
+++ _deploy_local.sh
@@ -0,0 +1,8 @@
+
+./_build.sh
+git clone -b gh-pages https://github.com/paleolimbot/qgis-intro.git book-output
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
